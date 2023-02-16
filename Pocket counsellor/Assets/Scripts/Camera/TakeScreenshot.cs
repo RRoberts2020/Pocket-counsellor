19e00cdceab4a2072d45fdf6e362296503975043
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2023-02-16 06:05:20 +0000

    Attempted to save images to phone gallery
    I attempted to improve upon my phone camera feature by having it save to the phone's actual gallery not the app data storage.
    
    Although there are no errors in the code and inspector , testing has proven that this method doesn't work.

--- Pocket counsellor/Assets/Scripts/Camera/TakeScreenshot.cs
+++ Pocket counsellor/Assets/Scripts/Camera/TakeScreenshot.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class TakeScreenshot : MonoBehaviour {
 
@@ -15,11 +16,23 @@ public class TakeScreenshot : MonoBehaviour {
 	IEnumerator CaptureIt()
 	{
 		string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
-		string fileName = "Screenshot" + timeStamp + ".png";
-		string pathToSave = fileName;
-		ScreenCapture.CaptureScreenshot(pathToSave);
 		yield return new WaitForEndOfFrame();
-		Instantiate (blink, new Vector2(0f, 0f), Quaternion.identity);
-	}
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
 
+		string filePath = Path.Combine(Application.persistentDataPath, "Picture" + timeStamp + ".png");
+		File.WriteAllBytes(GetAndroidExternalStoragePath(), ss.EncodeToPNG());
+	}
+	private string GetAndroidExternalStoragePath()
+	{
+		if (Application.platform != RuntimePlatform.Android)
+			return Application.persistentDataPath;
+
+		var jc = new AndroidJavaClass("android.os.Environment");
+		var path = jc.CallStatic<AndroidJavaObject>("getExternalStoragePublicDirectory",
+			jc.GetStatic<string>("DIRECTORY_DCIM"))
+			.Call<string>("getAbsolutePath");
+		return path;
+	}
 }
